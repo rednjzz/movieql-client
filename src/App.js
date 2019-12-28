9a8d2f31ab9277fd7f53cc3166987ff8bfa1c82d
rednjzz <rednjzz@gmail.com>
2019-12-28 20:51:10 +0900

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { ApolloProvider } from "react-apollo";
+import client from "./apolloClient";
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client= {client}> 
+        <div className="App" />
+      </ApolloProvider>
+    )
+  }
+}
+
+export default App;
